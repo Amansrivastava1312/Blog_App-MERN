3ef3c3bd9ae6302548ec084a459098fbaef1788b
Aman Srivastava <amanrajasthan123@gmail.com>
2024-02-11 20:22:07 +0530

    User SignIn(Api+Ui)

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -33,8 +33,8 @@ export default function SignUp() {
         navigate('/sign-in');
       }
     } catch (error) {
-      // setErrorMessage(error.message);
-      // setLoading(false);
+      setErrorMessage(error.message);
+      setLoading(false);
     }
   };
   return (
