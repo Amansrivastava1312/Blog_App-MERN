760fc6618d096ea920e98a77463c161106690d19
Aman Srivastava <amanrajasthan123@gmail.com>
2024-02-15 11:56:59 +0530

    user update API

--- api/index.js
+++ api/index.js
@@ -3,10 +3,11 @@ import express from "express";
 import mongoose from "mongoose";
 import userRoutes from "./routes/user.route.js"
 import authRoutes from "./routes/auth.route.js"
-
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 const PORT = 3000;
 import dotenv from "dotenv";
 dotenv.config();
