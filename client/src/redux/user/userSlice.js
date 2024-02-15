b0daf8f9e8f474a04eb5609e2257b86af7311102
Aman Srivastava <amanrajasthan123@gmail.com>
2024-02-15 19:33:52 +0530

    Delete account functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -35,7 +35,19 @@ const userSlice = createSlice({
     updateFailure:(state,action)=>{
       state.loading=false;
       state.error=action.payload;
-    }
+    }, deleteUserStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
    
   },
 });
@@ -46,7 +58,10 @@ export const {
   signInFailure,
   updateStart,
   updateSuccess,
-  updateFailure
+  updateFailure,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
   
 } = userSlice.actions;
 
