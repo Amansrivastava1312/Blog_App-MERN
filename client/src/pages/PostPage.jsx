eedb96eea59b95851599bb5558c3fc230b2a2a2e
Aman Srivastava <amanrajasthan123@gmail.com>
2024-03-10 01:29:56 +0530

    Add call to section

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,6 +3,7 @@ import { set } from 'mongoose';
 import React ,{useEffect,useState}from 'react'
 import { Link, useParams } from 'react-router-dom'
 import PostCard from '../components/PostCard';
+import CallToAction from '../components/CallToAction';
 
 export default function PostPage() {
   const { postSlug } = useParams();
@@ -84,10 +85,10 @@ export default function PostPage() {
         className='p-3 max-w-2xl mx-auto w-full post-content'
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
-      {/* <div className='max-w-4xl mx-auto w-full'>
+      <div className='max-w-4xl mx-auto w-full'>
         <CallToAction />
       </div>
-      <CommentSection postId={post._id} /> */}
+      {/* <CommentSection postId={post._id} /> */}
 
 
       <div className='flex flex-col justify-center items-center mb-5'>
