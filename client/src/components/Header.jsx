2b596bd39d5534691dd91a70e840c70875ead29b
Aman Srivastava <amanrajasthan123@gmail.com>
2024-03-10 23:34:51 +0530

    Added search and home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import {Link, useLocation} from 'react-router-dom'
-import {Avatar, Button, Dropdown, Navbar, NavbarToggle, TextInput} from 'flowbite-react'
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon, FaSun } from 'react-icons/fa';
-import {useSelector , useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
-import { signoutSuccess} from '../redux/user/userSlice'
+import { signoutSuccess } from '../redux/user/userSlice';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
   const path = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const {currentUser} = useSelector((state)=>state.user);
-  const {theme} = useSelector((state)=>state.theme)
+  const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   const handleSignout = async () => {
     try {
       const res = await fetch('/api/user/signout', {
@@ -27,49 +39,57 @@ export default function Header() {
       console.log(error.message);
     }
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
     <Navbar className='border-b-2'>
-        <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white' >
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Aman's</span>
-            Blog
-        
-        </Link>
-        <form>
-          <TextInput
+      <Link
+        to='/'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+      >
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+          Sahand's
+        </span>
+        Blog
+      </Link>
+      <form onSubmit={handleSubmit}>
+        <TextInput
           type='text'
-          placeholder='search...'
+          placeholder='Search...'
           rightIcon={AiOutlineSearch}
-          className='hidden lg:inline'/>
-        </form>
-        <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+          className='hidden lg:inline'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
         <AiOutlineSearch />
       </Button>
       <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline'
+        <Button
+          className='w-12 h-10 hidden sm:inline'
           color='gray'
           pill
-          onClick={()=>{
-            dispatch(toggleTheme());
-          }}>
-            {
-              theme==='dark'? <FaMoon/>:<FaSun/>
-            }
-          
-        </Button>
-        
-      {
-        
-        currentUser ? (<Dropdown
-        arrowIcon={false}
-        inline
-        label={
-          <Avatar
-          alt='user'
-          img={currentUser.profilePicture}
-          rounded/>
-        }
+          onClick={() => dispatch(toggleTheme())}
         >
-          <Dropdown.Header>
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
+        </Button>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
               <span className='block text-sm'>@{currentUser.username}</span>
               <span className='block text-sm font-medium truncate'>
                 {currentUser.email}
@@ -80,33 +100,27 @@ export default function Header() {
             </Link>
             <Dropdown.Divider />
             <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
-
-        </Dropdown>
-        ):
-        (
+          </Dropdown>
+        ) : (
           <Link to='/sign-in'>
             <Button gradientDuoTone='purpleToBlue' outline>
               Sign In
             </Button>
-      </Link>
-        )
-      }
-      <NavbarToggle/>
-
-    
+          </Link>
+        )}
+        <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link  as={'div'} active={path=='/'}>
+        <Navbar.Link active={path === '/'} as={'div'}>
           <Link to='/'>Home</Link>
         </Navbar.Link>
-        <Navbar.Link  as={'div'} active={path=='/about'}>
+        <Navbar.Link active={path === '/about'} as={'div'}>
           <Link to='/about'>About</Link>
         </Navbar.Link>
-        <Navbar.Link  as={'div'} active={path=='/projects'}>
+        <Navbar.Link active={path === '/projects'} as={'div'}>
           <Link to='/projects'>Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
-
     </Navbar>
-  )
+  );
 }
